4f495dbbea384527b9952fdf65e60b7e144d1185
Benedict Eastaugh <benedict@eastaugh.net>
2010-07-27 21:29:20 +0100

    fn -> f.

--- src/udon.js
+++ src/udon.js
@@ -1,37 +1,37 @@
 Udon = (typeof Udon === 'undefined') ? {} : Udon;
 
-Udon.foldl = function(fn, z, xs) {
+Udon.foldl = function(f, z, xs) {
     var len = xs.length, i;
-    for (i = 0; i < len; i++) z = fn(z, xs[i]);
+    for (i = 0; i < len; i++) z = f(z, xs[i]);
     return z;
 };
 
-Udon.foldr = function(fn, z, xs) {
+Udon.foldr = function(f, z, xs) {
     var i = xs.length;
-    while (i--) z = fn(xs[i], z);
+    while (i--) z = f(xs[i], z);
     return z;
 };
 
-Udon.map = function(fn, xs) {
+Udon.map = function(f, xs) {
     var i = xs.length, ys = new Array(i);
-    while (i--) ys[i] = fn(xs[i]);
+    while (i--) ys[i] = f(xs[i]);
     return ys;
 };
 
-Udon.filter = function(fn, xs) {
+Udon.filter = function(f, xs) {
     var ys = [], len = xs.length, i, e;
     for (i = 0; i < len; i++) {
         e = xs[i];
-        if (fn(e)) ys.push(e);
+        if (f(e)) ys.push(e);
     }
     return ys;
 };
 
-Udon.partition = function(fn, xs) {
+Udon.partition = function(f, xs) {
     var ts = [], fs = [], len = xs.length, i, e;
     for (i = 0; i < len; i++) {
         e = xs[i];
-        (fn(e) ? ts : fs).push(e);
+        (f(e) ? ts : fs).push(e);
     }
     return [ts, fs];
 };
@@ -49,9 +49,9 @@ Udon.zip = function(xs, ys) {
     return Udon.zipWith(function(x, y) { return [x, y]; }, xs, ys);
 };
 
-Udon.zipWith = function(fn, xs, ys) {
+Udon.zipWith = function(f, xs, ys) {
     var xsl = xs.length, ysl = ys.length, i = xsl > ysl ? ysl : xsl,
         zs = new Array(i);
-    while (i--) zs[i] = fn(xs[i], ys[i]);
+    while (i--) zs[i] = f(xs[i], ys[i]);
     return zs;
 };
