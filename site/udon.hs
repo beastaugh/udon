a01e3f1c2e3a22c42483da615218afc8f59f524e
Benedict Eastaugh <benedict@eastaugh.net>
2011-04-01 18:50:03 +0100

    Update site to use Hakyll 3.

--- site/udon.hs
+++ site/udon.hs
@@ -1,15 +1,29 @@
-module Main (main) where
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
 
-import Text.Hakyll (hakyll)
-import Text.Hakyll.File (directory)
-import Text.Hakyll.Render (renderChain, static)
-import Text.Hakyll.CreateContext (createPage)
+import Control.Arrow ((>>>))
+import Control.Monad (forM_)
 
-main = hakyll "http://extralogical.net/projects/udon" $ do
-  static "udon.css"
-  directory static "downloads"
-  
-  mapM_ (renderChain [ "templates/default.html" ] . createPage)
-          [ "index.md"
+import Hakyll
+
+main :: IO ()
+main = hakyll $ do
+    -- Read templates
+    compile "templates/*" templateCompiler
+    
+    -- Compress CSS
+    route   "css/*" idRoute
+    compile "css/*" compressCssCompiler
+    
+    -- Archive downloads
+    route   "downloads/*" idRoute
+    compile "downloads/*" copyFileCompiler
+    
+    -- Documentation pages
+    forM_ [ "index.md"
           , "license.md"
-          ]
+          ] $ \page -> do
+        route   page $ setExtension ".html"
+        compile page $ pageCompiler
+            >>> applyTemplateCompiler "templates/default.html"
+            >>> relativizeUrlsCompiler
