a5cdd45864bf959df0ffaabf492cd5c7fef09714
Benedict Eastaugh <benedict@eastaugh.net>
2012-08-29 14:57:39 +0100

    Tidy existing codebase.

--- src/udon.js
+++ src/udon.js
@@ -6,18 +6,23 @@ Udon._slice = Array.prototype.slice;
 
 Udon.ncurry = function(n) {
     if (typeof n != 'number' || n < 2) n = 2;
+    
     var curry = function(f) {
         var a1 = Udon._slice.call(arguments, 1);
+        
         return function() {
             var a2 = a1.concat(Udon._slice.call(arguments, 0));
+            
             if (a2.length < n) {
                 a2.unshift(f);
+                
                 return curry.apply(null, a2);
             } else {
                 return f.apply(null, a2);
             }
         };
     };
+    
     return curry;
 };
 
@@ -25,23 +30,35 @@ Udon.curry = function(f) {
     var ar = f.length, a1 = Udon._slice.call(arguments, 1),
     accumulator = function() {
         var a2 = a1.concat(Udon._slice.call(arguments, 0));
+        
         return a2.length >= ar ?
-            f.apply(null, a2) : Udon.curry.apply(null, [f].concat(a2));
+            f.apply(null, a2) :
+            Udon.curry.apply(null, [f].concat(a2));
     };
+    
     return a1.length >= ar ? accumulator() : accumulator;
 };
 
 Udon.compose = function(fs, ar) {
     var composed = function() {
-        var i  = fs.length - 1, x,
-            as = Udon._slice.call(arguments, 0);
+        var as = Udon._slice.call(arguments, 0),
+            i  = fs.length - 1,
+            x;
+        
         if (i < 0) return as[0];
+        
         x = fs[i].apply(null, as);
-        while (i--) x = fs[i](x);
+        
+        while (i--) {
+            x = fs[i](x);
+        }
+        
         return x;
     };
+    
     return typeof ar == 'number' && ar > 1 ?
-        Udon.ncurry(ar)(composed) : composed;
+        Udon.ncurry(ar)(composed) :
+        composed;
 };
 
 Udon.id = function(x) {
@@ -49,20 +66,35 @@ Udon.id = function(x) {
 };
 
 Udon.foldl = function(f, z, xs) {
-    var len = xs.length, i;
-    for (i = 0; i < len; i++) z = f(z, xs[i]);
+    var len = xs.length,
+        i;
+    
+    for (i = 0; i < len; i++) {
+        z = f(z, xs[i]);
+    }
+    
     return z;
 };
 
 Udon.foldl1 = function(f, xs) {
-    var len = xs.length, z = xs[0], i;
-    for (i = 1; i < len; i++) z = f(z, xs[i]);
+    var len = xs.length,
+        z   = xs[0],
+        i;
+    
+    for (i = 1; i < len; i++) {
+        z = f(z, xs[i]);
+    }
+    
     return z;
 };
 
 Udon.foldr = function(f, z, xs) {
     var i = xs.length;
-    while (i--) z = f(xs[i], z);
+    
+    while (i--) {
+        z = f(xs[i], z);
+    }
+    
     return z;
 };
 
@@ -81,36 +113,61 @@ Udon.minimum = function(xs) {
 };
 
 Udon.maximumBy = function(cmp, xs) {
-    var maxBy = function(x, y) { return cmp(x, y) === 1 ? x : y; };
+    var maxBy = function(x, y) {
+        return cmp(x, y) === 1 ? x : y;
+    };
+    
     return Udon.foldl1(maxBy, xs);
 };
 
 Udon.minimumBy = function(cmp, xs) {
-    var minBy = function(x, y) { return cmp(x, y) === 1 ? y : x; };
+    var minBy = function(x, y) {
+        return cmp(x, y) === 1 ? y : x;
+    };
+    
     return Udon.foldl1(minBy, xs);
 };
 
 Udon.map = function(f, xs) {
-    var i = xs.length, ys = new Array(i);
-    while (i--) ys[i] = f(xs[i]);
+    var i  = xs.length,
+        ys = new Array(i);
+    
+    while (i--) {
+        ys[i] = f(xs[i]);
+    }
+    
     return ys;
 };
 
 Udon.reverse = function(xs) {
-    var len = xs.length, ys, i;
+    var len = xs.length,
+        ys, i;
+    
     if (len < 1) return xs;
+    
     ys = new Array(len);
     i  = len;
-    while (i--) ys[i] = xs[len - i - 1];
+    
+    while (i--) {
+        ys[i] = xs[len - i - 1];
+    }
+    
     return ys;
 };
 
 Udon.intersperse = function(sep, xs) {
-    var i = xs.length;
+    var i = xs.length,
+        ys;
+    
     if (i < 2) return xs;
-    i = i * 2 - 1;
+    
+    i  = i * 2 - 1;
     ys = new Array(i);
-    while (i--) ys[i] = i % 2 === 0 ? xs[Math.floor(i / 2)] : sep;
+    
+    while (i--) {
+        ys[i] = i % 2 === 0 ? xs[Math.floor(i / 2)] : sep;
+    }
+    
     return ys;
 };
 
@@ -119,27 +176,35 @@ Udon.intercalate = function(xs, xss) {
 };
 
 Udon.filter = function(f, xs) {
-    var ys = [], len = xs.length, i, e;
+    var ys  = [],
+        len = xs.length,
+        i, e;
+    
     for (i = 0; i < len; i++) {
         e = xs[i];
         if (f(e)) ys.push(e);
     }
+    
     return ys;
 };
 
 Udon.any = function(p, xs) {
     var i = xs.length;
+    
     while (i--) {
         if (p(xs[i])) return true;
     }
+    
     return false;
 };
 
 Udon.all = function(p, xs) {
     var i = xs.length;
+    
     while (i--) {
         if (!p(xs[i])) return false;
     }
+    
     return true;
 };
 
@@ -148,44 +213,70 @@ Udon.none = function(p, xs) {
 };
 
 Udon.sum = function(ns) {
-    var i = ns.length, t = 0;
-    while (i--) t += ns[i];
+    var i = ns.length,
+        t = 0;
+    
+    while (i--) {
+        t += ns[i];
+    }
+    
     return t;
 };
 
 Udon.product = function(ns) {
-    var i = ns.length, t = 1;
-    while (i--) t *= ns[i];
+    var i = ns.length,
+        t = 1;
+    
+    while (i--) {
+        t *= ns[i];
+    }
+    
     return t;
 };
 
 Udon.elem = function(e, xs) {
     var i = xs.length;
-    while (i--) if (e === xs[i]) return true;
+    
+    while (i--) {
+        if (e === xs[i]) return true;
+    }
+    
     return false;
 };
 
 Udon.notElem = function(e, xs) {
-    var i = xs.length, notInList = true;
-    while (i--) if (e === xs[i]) notInList = false;
-    return notInList;
+    var i = xs.length;
+    
+    while (i--) {
+        if (e === xs[i]) return false;
+    }
+    
+    return true;
 };
 
 Udon.partition = function(f, xs) {
-    var ts = [], fs = [], len = xs.length, i, e;
+    var ts  = [],
+        fs  = [],
+        len = xs.length,
+        i, e;
+    
     for (i = 0; i < len; i++) {
         e = xs[i];
         (f(e) ? ts : fs).push(e);
     }
+    
     return [ts, fs];
 };
 
 Udon.unfoldr = function(step, seed) {
-    var output  = [], result;
+    var output = [],
+        result;
+    
     while ((result = step(seed))) {
         output.push(result[0]);
         seed = result[1];
     }
+    
     return output;
 };
 
@@ -194,15 +285,21 @@ Udon.zip = function(xs, ys) {
 };
 
 Udon.zipWith = function(f, xs, ys) {
-    var xsl = xs.length, ysl = ys.length, i = xsl > ysl ? ysl : xsl,
-        zs = new Array(i);
-    while (i--) zs[i] = f(xs[i], ys[i]);
+    var xsl = xs.length,
+        ysl = ys.length,
+        i   = xsl > ysl ? ysl : xsl,
+        zs  = new Array(i);
+    
+    while (i--) {
+        zs[i] = f(xs[i], ys[i]);
+    }
+    
     return zs;
 };
 
-
 Udon.and = function(xs) {
-    var len = xs.length, i;
+    var len = xs.length,
+        i;
     
     for (i = 0; i < len; i++) {
         if (xs[i] === false) return false;
@@ -236,7 +333,8 @@ Udon.drop = function(n, xs) {
 };
 
 Udon.dropWhile = function(pred, xs) {
-    var i, len = xs.length;
+    var len = xs.length,
+        i;
     
     if (pred(xs[0]) === false) return xs;
     
@@ -270,7 +368,8 @@ Udon.empty = function(xs) {
 };
 
 Udon.equal = function(xs, ys) {
-    var xlen = xs.length, ylen = ys.length, i;
+    var xsl = xs.length,
+        ysl = ys.length, i;
     
     if (typeof xs !== typeof ys) {
         return false;
@@ -278,9 +377,9 @@ Udon.equal = function(xs, ys) {
         return xs === ys;
     }
     
-    if (xlen !== ylen) return false;
+    if (xsl !== ysl) return false;
     
-    for (i = 0; i < xlen; i++) {
+    for (i = 0; i < xsl; i++) {
         if (Udon.equal(xs[i], ys[i]) === false) return false;
     }
     
@@ -288,7 +387,8 @@ Udon.equal = function(xs, ys) {
 };
 
 Udon.find = function(pred, xs) {
-    var len = xs.length, i;
+    var len = xs.length,
+        i;
     
     for (i = 0; i < len; i++) {
         if (pred(xs[i])) return xs[i];
